afb24d395fe32ae9bc67c33c48427f881dff0434
Shantanu Mehta <shantanumehta27@gmail.com>
2025-06-26 12:48:24 +0530

    Update evaluator.h

--- evaluator.h
+++ evaluator.h
@@ -3,8 +3,11 @@
 
 #include "tokenizer.h"
 
-int evaluate_expression(Token* tokens, int* index);
-int evaluate_condition(Token* tokens, int* index);
 void evaluate_tokens(Token* tokens, int token_count);
 
-#endif
+int evaluate_expression(Token* tokens, int* index);
+
+
+int evaluate_condition(Token* tokens, int* index);
+
+#endif 
