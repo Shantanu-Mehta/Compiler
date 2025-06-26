98bf32d06fcab2629e408a1645e529c4fbdf7e35
Shantanu Mehta <shantanumehta27@gmail.com>
2025-06-26 12:52:27 +0530

    Update tokenizer.h

--- tokenizer.h
+++ tokenizer.h
@@ -1,25 +1,30 @@
 #ifndef TOKENIZER_H
 #define TOKENIZER_H
 
-#define MAX_TOKENS 1024
+#define MAX_TOKENS 2048
+#define MAX_TOKEN_LENGTH 64
+
 
 typedef enum {
-    TOKEN_NUMBER,
-    TOKEN_IDENTIFIER,
-    TOKEN_KEYWORD,
-    TOKEN_OPERATOR,
-    TOKEN_SYMBOL,
-    TOKEN_EOF
+    TOKEN_NUMBER,         
+    TOKEN_IDENTIFIER,     
+    TOKEN_KEYWORD,        
+    TOKEN_OPERATOR,      
+    TOKEN_SYMBOL,         
+    TOKEN_STRING,         
+    TOKEN_PREPROCESSOR,   
+    TOKEN_EOF             
 } TokenType;
+
 
 typedef struct {
     TokenType type;
-    char value[64];
+    char value[MAX_TOKEN_LENGTH];
 } Token;
 
+
 Token* tokenize(const char* input, int* token_count);
-
 const char* token_type_to_string(int type);
+void print_tokens(Token* tokens, int count);
 
-
-#endif
+#endif 
