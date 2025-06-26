e4c6c569833cc4e3b4e83723210e192387714d72
Shantanu Mehta <shantanumehta27@gmail.com>
2025-06-26 12:52:11 +0530

    Update tokenizer.c

--- tokenizer.c
+++ tokenizer.c
@@ -3,67 +3,49 @@
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
+#include <stdbool.h>
 
-int is_operator_char(char c) {
+bool is_operator_char(char c) {
     return c == '+' || c == '-' || c == '*' || c == '/' || c == '=' ||
-           c == '<' || c == '>' || c == '!';
+           c == '<' || c == '>' || c == '!' || c == '%';
 }
 
-int is_symbol_char(char c) {
-    return c == ';' || c == '{' || c == '}' || c == '(' || c == ')' || c == ',' || c == '[' || c == ']';
+bool is_symbol_char(char c) {
+    return c == ';' || c == '{' || c == '}' || c == '(' || c == ')' ||
+           c == ',' || c == '[' || c == ']' || c == '.' || c == ':';
 }
-
 
 int is_keyword(const char* str) {
-    return strcmp(str, "auto") == 0 ||
-            strcmp(str, "print") == 0 || 
-           strcmp(str, "break") == 0 ||
-           strcmp(str, "case") == 0 ||
-           strcmp(str, "char") == 0 ||
-           strcmp(str, "const") == 0 ||
-           strcmp(str, "continue") == 0 ||
-           strcmp(str, "default") == 0 ||
-           strcmp(str, "do") == 0 ||
-           strcmp(str, "double") == 0 ||
-           strcmp(str, "else") == 0 ||
-           strcmp(str, "enum") == 0 ||
-           strcmp(str, "extern") == 0 ||
-           strcmp(str, "float") == 0 ||
-           strcmp(str, "for") == 0 ||
-           strcmp(str, "goto") == 0 ||
-           strcmp(str, "if") == 0 ||
-           strcmp(str, "inline") == 0 ||
-           strcmp(str, "int") == 0 ||
-           strcmp(str, "long") == 0 ||
-           strcmp(str, "register") == 0 ||
-           strcmp(str, "restrict") == 0 ||
-           strcmp(str, "return") == 0 ||
-           strcmp(str, "short") == 0 ||
-           strcmp(str, "signed") == 0 ||
-           strcmp(str, "sizeof") == 0 ||
-           strcmp(str, "static") == 0 ||
-           strcmp(str, "struct") == 0 ||
-           strcmp(str, "switch") == 0 ||
-           strcmp(str, "typedef") == 0 ||
-           strcmp(str, "union") == 0 ||
-           strcmp(str, "unsigned") == 0 ||
-           strcmp(str, "void") == 0 ||
-           strcmp(str, "volatile") == 0 ||
-           strcmp(str, "while") == 0;
+    return strcmp(str, "print") == 0 || strcmp(str, "printf") == 0 ||
+           strcmp(str, "if") == 0 || strcmp(str, "else") == 0 ||
+           strcmp(str, "for") == 0 || strcmp(str, "while") == 0 ||
+           strcmp(str, "return") == 0 || strcmp(str, "int") == 0 ||
+           strcmp(str, "float") == 0 || strcmp(str, "char") == 0 ||
+           strcmp(str, "void") == 0 || strcmp(str, "main") == 0 ||
+           strcmp(str, "include") == 0;
 }
+
 const char* token_type_to_string(int type) {
-    switch(type) {
+    switch (type) {
         case TOKEN_NUMBER: return "NUMBER";
         case TOKEN_IDENTIFIER: return "IDENTIFIER";
         case TOKEN_OPERATOR: return "OPERATOR";
         case TOKEN_SYMBOL: return "SYMBOL";
         case TOKEN_KEYWORD: return "KEYWORD";
+        case TOKEN_STRING: return "STRING";
+        case TOKEN_PREPROCESSOR: return "PREPROCESSOR";
         case TOKEN_EOF: return "EOF";
         default: return "UNKNOWN";
     }
 }
-    
 
+void print_tokens(Token* tokens, int count) {
+    printf("=== TOKENS ===\n");
+    for (int i = 0; i < count; i++) {
+        printf("Type: %-12s, Value: %s\n", token_type_to_string(tokens[i].type), tokens[i].value);
+    }
+    printf("\n");
+}
 
 Token* tokenize(const char* input, int* token_count) {
     Token* tokens = malloc(sizeof(Token) * MAX_TOKENS);
@@ -81,108 +63,130 @@ Token* tokenize(const char* input, int* token_count) {
             continue;
         }
 
-        // Numbers (integers only)
-        if (isdigit(input[i])) {
+        if (count >= MAX_TOKENS - 1) {
+            fprintf(stderr, "Token limit exceeded\n");
+            exit(1);
+        }
+
+        // Preprocessor directive
+        if (input[i] == '#') {
+            int start = i;
+            while (input[i] != '\n' && input[i] != '\0') i++;
+            int length = i - start;
+            strncpy(tokens[count].value, input + start, length);
+            tokens[count].value[length] = '\0';
+            tokens[count].type = TOKEN_PREPROCESSOR;
+            count++;
+            if (input[i] == '\n') i++;
+            continue;
+        }
+
+        
+        if (input[i] == '/' && input[i + 1] == '/') {
+            i += 2;
+            while (input[i] != '\n' && input[i] != '\0') i++;
+            continue;
+        }
+
+        
+        if (input[i] == '/' && input[i + 1] == '*') {
+            i += 2;
+            while (input[i] && !(input[i] == '*' && input[i + 1] == '/')) i++;
+            if (input[i]) i += 2;
+            continue;
+        }
+
+        
+        if (input[i] == '"') {
+            i++;
+            char str[256];
             int j = 0;
+            while (input[i] != '"' && input[i] != '\0') {
+                if (j < 255) str[j++] = input[i++];
+                else i++;
+            }
+            str[j] = '\0';
+            if (input[i] == '"') i++;
+
+            tokens[count].type = TOKEN_STRING;
+            strncpy(tokens[count].value, str, MAX_TOKEN_LENGTH - 1);
+            tokens[count].value[MAX_TOKEN_LENGTH - 1] = '\0';
+            count++;
+            continue;
+        }
+
+        
+        if (isdigit(input[i])) {
             char number[64];
+            int j = 0;
             while (isdigit(input[i])) {
-                if (j < 63) number[j++] = input[i];
-                i++;
+                if (j < 63) number[j++] = input[i++];
+                else i++;
             }
             number[j] = '\0';
 
-            if (count >= MAX_TOKENS) {
-                fprintf(stderr, "Error: Too many tokens. Increase MAX_TOKENS.\n");
-                exit(1);
-            }
-
             tokens[count].type = TOKEN_NUMBER;
-            strcpy(tokens[count].value, number);
+            strncpy(tokens[count].value, number, MAX_TOKEN_LENGTH - 1);
+            tokens[count].value[MAX_TOKEN_LENGTH - 1] = '\0';
             count++;
             continue;
         }
 
-    
+        
         if (isalpha(input[i]) || input[i] == '_') {
-            int j = 0;
             char ident[64];
+            int j = 0;
             while (isalnum(input[i]) || input[i] == '_') {
-                if (j < 63) ident[j++] = input[i];
-                i++;
+                if (j < 63) ident[j++] = input[i++];
+                else break;
             }
             ident[j] = '\0';
 
-            if (count >= MAX_TOKENS) {
-                fprintf(stderr, "Error: Too many tokens. Increase MAX_TOKENS.\n");
-                exit(1);
-            }
-
             tokens[count].type = is_keyword(ident) ? TOKEN_KEYWORD : TOKEN_IDENTIFIER;
-            strcpy(tokens[count].value, ident);
+            strncpy(tokens[count].value, ident, MAX_TOKEN_LENGTH - 1);
+            tokens[count].value[MAX_TOKEN_LENGTH - 1] = '\0';
             count++;
             continue;
         }
 
-       
+        
         if ((input[i] == '=' && input[i+1] == '=') ||
             (input[i] == '!' && input[i+1] == '=') ||
             (input[i] == '<' && input[i+1] == '=') ||
             (input[i] == '>' && input[i+1] == '=')) {
-
-            if (count >= MAX_TOKENS) {
-                fprintf(stderr, "Error: Too many tokens. Increase MAX_TOKENS.\n");
-                exit(1);
-            }
-
             tokens[count].type = TOKEN_OPERATOR;
             tokens[count].value[0] = input[i];
-            tokens[count].value[1] = input[i+1];
+            tokens[count].value[1] = input[i + 1];
             tokens[count].value[2] = '\0';
             i += 2;
             count++;
             continue;
         }
 
-        // Single character operator
+        
         if (is_operator_char(input[i])) {
-            if (count >= MAX_TOKENS) {
-                fprintf(stderr, "Error: Too many tokens. Increase MAX_TOKENS.\n");
-                exit(1);
-            }
-
             tokens[count].type = TOKEN_OPERATOR;
-            tokens[count].value[0] = input[i];
+            tokens[count].value[0] = input[i++];
             tokens[count].value[1] = '\0';
-            i++;
             count++;
             continue;
         }
 
-        // Symbols
+        
         if (is_symbol_char(input[i])) {
-            if (count >= MAX_TOKENS) {
-                fprintf(stderr, "Error: Too many tokens. Increase MAX_TOKENS.\n");
-                exit(1);
-            }
-
             tokens[count].type = TOKEN_SYMBOL;
-            tokens[count].value[0] = input[i];
+            tokens[count].value[0] = input[i++];
             tokens[count].value[1] = '\0';
-            i++;
             count++;
             continue;
         }
 
-       
-        fprintf(stderr, "Unknown character: %c\n", input[i]);
-        exit(1);
+      
+        fprintf(stderr, "Skipping unknown character: %c\n", input[i]);
+        i++;
     }
 
-    if (count >= MAX_TOKENS) {
-        fprintf(stderr, "Error: Too many tokens. Increase MAX_TOKENS.\n");
-        exit(1);
-    }
-
+   
     tokens[count].type = TOKEN_EOF;
     strcpy(tokens[count].value, "EOF");
     count++;
